803cd0449841868cac026bb46e4338d9ac2bc3f6
tudor <tudoras1970@gmail.com>
2020-10-16 09:48:48 +0300

    use new url parser

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const config = require("./config");
 
 const DBObject = {
-    dbInstance: mongoose.connect(config.CONNECTION_STRING),
+    dbInstance: mongoose.connect(config.CONNECTION_STRING, {useNewUrlParser: true}),
     
     _postareInternals: {
         schema: undefined,
